7d4ed63af444e75a4cd8883187f65b956971edff
johnazre <jaboyak@gmail.com>
2016-06-05 13:35:08 -0700

    still needs prettying up and query capabilities

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 var app = angular.module('testApp', [])
 
-.controller('mainCtrl', function($scope) {
-  $scope.someshit = "hello world"
+.controller('mainCtrl', function($scope, mainSvc) {
+
+  $scope.getData = function() {
+    mainSvc.getData($scope.searchTerm).then(function(res){
+      console.log(res);
+      $scope.data = res;
+    });
+
+
+  }
+
 })
+
+.service("mainSvc", function($http, $q){
+
+  this.getData = function(searchTerm){
+    var defer = $q.defer();
+
+    $http({
+      method: "GET",
+      url: "http://swapi.co/api/people/1"
+    }).then(function(res){
+      defer.resolve(res.data)
+    })
+
+    return defer.promise;
+  }
+
+});
